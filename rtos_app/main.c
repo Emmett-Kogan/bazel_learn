de6d80e8ecd65e576ad599fc618a949189fab8c9
Emmett-Kogan <emmett.kogan@gmail.com>
2025-12-09 00:00:52 -0500

    Fixed submodules. Note to self, never rename them again
    builds working with a variable to select heap. Should work with static only too since there is an option to just not use a heap.

--- rtos_app/main.c
+++ rtos_app/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "semphr.h"
+
+static void task(void *params) {
+    for (;;) {
+        printf("Hello, World!\n");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
+
 int main() {
-    printf("Hello, World!\n");
+    if (pdPASS != xTaskCreate(task, "task", 2048, NULL, 1, NULL)) {
+        printf("Failed to schedule task");
+    }
+
+    printf("Starting scheduler...\n");
+    vTaskStartScheduler();
+
     return 0;
 }
